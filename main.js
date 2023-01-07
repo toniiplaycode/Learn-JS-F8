d82fa9bf7926906cddc0ee25887b57f186f9638e
toniiwithJS <toan13022017@gmail.com>
2023-01-07 16:14:42 +0700

    update main.js file & note.txt file

--- main.js
+++ main.js
@@ -55,19 +55,47 @@
 // }
 // console.log(myInfo.getName()); //gọi function trong object myInfo
 
-//ex8: object constructor
-function User(firstName, lastName, avatar){ // tên object constructor phải viết hoa chữ cái đầu (quy ước)
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.avatar = avatar;
-    this.getFullName = function() {
-        return this.firstName + this.lastName;
-    }
-};
-var admin = new User('Thanh', 'Toan', 'avatar1');
-var client =  new User('Minh', 'Hung', 'avatar2');
-admin.age = 20; // thêm thuộc tính riêng của admin
-client.address = 'Ho Chi Minh'; // thêm thuộc tính riêng của client
-delete admin.avatar // xoá thuộc tính avatar của admin
-console.log(admin, client);
-console.log(admin.getFullName(), client.getFullName())
+// //ex8: object constructor
+// function User(firstName, lastName, avatar){ // tên object constructor phải viết hoa chữ cái đầu (quy ước)
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.avatar = avatar;
+//     this.getFullName = function() {
+//         return this.firstName + this.lastName;
+//     }
+// };
+// var admin = new User('Thanh', 'Toan', 'avatar1');
+// var client =  new User('Minh', 'Hung', 'avatar2');
+// admin.age = 20; // thêm thuộc tính riêng của admin
+// client.address = 'Ho Chi Minh'; // thêm thuộc tính riêng của client
+// delete admin.avatar // xoá thuộc tính avatar của admin
+// console.log(admin, client);
+// console.log(admin.getFullName(), client.getFullName())
+
+//ex9: objectt prototype
+// function User(firstName, lastName, avatar){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.avatar = avatar;
+// };
+// User.prototype.className = 'F8'; // tạo thêm thuộc tính bên ngoài constructor dùng prototype và các object điều sẽ được thừa hưởng
+// User.prototype.getLastName = function() { // tạo thêm phương thức bên ngoài constructor dùng prototype và các object điều sẽ được thừa hưởng
+//     return this.lastName;
+// }
+// var admin = new User('Thanh', 'Toan', 'avatar1');
+// var client =  new User('Minh', 'Hung', 'avatar2');
+// console.log(admin, client); // className = 'F8' nằm trong prototype của dev tool
+// console.log(admin.getLastName(), client.getLastName());
+
+//ex10: object Date
+var date = new Date();
+console.log(date);
++ date.getFullYear(); // lấy năm;
++ date.getMonth() + 1; // lấy tháng;
++ date.getDate(); // lấy ngày;
++ date.getHours(); // lấy giờ;
++ date.getMinutes(); // lấy phút;
++ date.getSeconds(); // lấy giây;
+console.log(date.getFullYear());
+console.log(date.getMonth() + 1);
+console.log(date.getDate());
