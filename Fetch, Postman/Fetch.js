1ae01a624270902cc6e26515fc056e12828d3095
toniiwithJS <toan13022017@gmail.com>
2023-02-03 19:05:16 +0700

    update some file & renamed file FetchNote.txt -> Fetch&JSONServer&PostmanNote.txt

--- Fetch, Postman/Fetch.js
+++ Fetch, Postman/Fetch.js
@@ -20,11 +20,70 @@
 //     })
 // 
 // ex2: lấy dữ liệu từ JSON server
+// var userAPI = 'http://localhost:3000/user';
+// fetch(userAPI)
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function(users){
+//         console.log(users);
+//     })
+//
+// ví dụ thêm, sửa, xoá API
 var userAPI = 'http://localhost:3000/user';
-fetch(userAPI)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(users){
-        console.log(users);
-    })
+function start(){ // hàm start để gọi các hàm con (hàm trong hàm)
+    getUsers(renderUsers); // khi get dữ liệu xong thì render luôn
+    handleCreateForm(); 
+}
+start(); // hàm start sẽ được chạy khi mở trình duyệt
+function getUsers(callback) { // hàm getUsers để lấy dữ liệu từ API
+    fetch(userAPI)
+        .then(function(response){
+            return response.json();
+        })
+        .then(callback); // dùng callback cho oai hehe ^^
+        /* thay vì .then(function(users){
+            console.log(users);
+        })  như này thì mình có thể dùng callback như trên  */
+}
+function renderUsers(users){ // hàm renderUsers để show các dữ liệu được lấy từ API lên HTML
+    console.log(users);
+    var listUsersBlock = document.querySelector('#list-users');
+    var htmls = users.map(function(user){
+        return `<li>
+                    <h3>Name: ${user.name}</h3>
+                    <p>ID: ${user.id}</p>
+                    <p>Address: ${user.address}</p>
+                </li>`;
+    });
+    return listUsersBlock.innerHTML = htmls.join(' ');
+}
+function handleCreateForm(){ // hàm xử lý form để lấy được value của input
+    var createBtn = document.querySelector('#btn-create');
+    createBtn.onclick = function(){ // khi click vào nút create thì sẽ lấy được value từ 2 input
+        var name = document.querySelector('input[name="name"]').value;
+        var address = document.querySelector('input[name="address"]').value;
+        var formData = {
+            name: name, // cái key phải giống với key trên API
+            address: address // cái key phải giống với key trên API
+        };
+        createUser(formData, function(){
+            getUsers(renderUsers);
+        });  
+    }
+}
+// sau khi lấy được value từ 2 input, sau đó sẽ gửi yêu cầu method POST để thêm được dữ liệu 
+function createUser(data, callback){ // hàm createUsers để gửi yêu cầu thêm dữ liệu, tham số data truyền vào dữ liệu sẽ thêm, callback để sau khi dữ liệu đã được thêm thì GET dữ liệu đó luôn 
+    var options = {  // mấy cái trong object này là làm theo video 179 của F8
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+    fetch(userAPI, options) // fetch bình thường nếu 1 tham số thì method mặc định là GET, nếu có thêm tham số thứ 2 (là 1 object) thì method là: POST, PUT/PATCH, DELETE
+        .then(function(response){
+           return response.json();
+        })
+        .then(callback);
+}
